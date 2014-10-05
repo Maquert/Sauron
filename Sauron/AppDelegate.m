b46b260c60abc032b9437f9a5ce47ad834e16b3a
Miguel Hernández Jaso <miguel@mhjaso.com>
2014-10-05 19:04:08 +0200

    AppDelegate - Examples added

--- Sauron/AppDelegate.m
+++ Sauron/AppDelegate.m
@@ -27,14 +27,18 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
+    // With TabBar (using MHJStoryboardBuilder)
     UITabBarController *tabVC = [MHJStoryboardBuilder tabBarControllerWithStoryboardNames:@[SB_FRODO, SB_GANDALF,SB_SAURON]];
     [self.window setRootViewController:tabVC];
     
+    // Setting a single NavigationController (using switchToStoryboardNamed:...)
     //[MHJSauron switchToStoryboardNamed:SB_FRODO withViewIdentifier:nil returningViewController:nil];
     
+    // Manually
     /*UIStoryboard *storyboard = [UIStoryboard storyboardWithName:SB_FRODO bundle:nil];
     UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:[storyboard instantiateViewControllerWithIdentifier:VIEW_IDENTIFIER_DETAIL]];
-    [self.window setRootViewController:navVC];*/
+    [self.window setRootViewController:navVC];
+     */
     
     [self.window makeKeyAndVisible];
     return YES;
